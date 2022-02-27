4cbcba50dd352681a691708d02413aaa05c94e8b
Mohamed Agamy <agamy207@gmail.com>
2022-02-27 02:33:42 +0200

    hotel_item

--- app/src/main/java/com/kemet/kemetapp/ui/HomeActivity.java
+++ app/src/main/java/com/kemet/kemetapp/ui/HomeActivity.java
@@ -1,21 +1,21 @@
 package com.kemet.kemetapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.kemet.kemetapp.Adapter.HomeAdapter;
+
 import com.kemet.kemetapp.R;
-import com.kemet.kemetapp.pojo.HomeModel;
-import com.shrikanthravi.customnavigationdrawer2.widget.SNavigationDrawer;
+
 
 public class HomeActivity extends AppCompatActivity {
-    SNavigationDrawer mNavigationDrawer;
-    ImageView mOpenMenu ;
+    DrawerLayout mNavigationDrawer;
+    ImageView mOpenMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +29,14 @@ public class HomeActivity extends AppCompatActivity {
 
     //iniView
     private void iniView() {
-        mOpenMenu=findViewById(R.id.meny_nav);
-        mNavigationDrawer = findViewById(R.id.navigationDrawer);
-
+        mOpenMenu = findViewById(R.id.meny_nav);
+        mNavigationDrawer = findViewById(R.id.NavigationDrawer);
 
 
         mOpenMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                mNavigationDrawer.openDrawer();
+                mNavigationDrawer.openDrawer(Gravity.LEFT);
 
             }
         });
--- app/src/main/java/com/kemet/kemetapp/ui/fragment/HotielFragment.java
+++ app/src/main/java/com/kemet/kemetapp/ui/fragment/HotielFragment.java
@@ -1,32 +1,42 @@
 package com.kemet.kemetapp.ui.fragment;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.kemet.kemetapp.Adapter.HomeAdapter;
 import com.kemet.kemetapp.R;
+import com.kemet.kemetapp.pojo.HomeModel;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HotielFragment extends Fragment {
+    //view
+    RecyclerView mHotelRecycler;
+
+    //Model
+    List<HomeModel> mList;
+    HomeAdapter mHotelAdapter;
+
+    //Firebase
+    FirebaseFirestore mFireStore;
 
     public HotielFragment() {
         // Required empty public constructor
     }
 
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,5 +49,42 @@ public class HotielFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        iniView(view);
+        getDataFromFirebase();
     }
+
+
+    private void iniView(View view) {
+
+        //View
+        mHotelRecycler = view.findViewById(R.id.recyclerHotiel);
+        //
+        mList = new ArrayList<>();
+        //Firebase
+        mFireStore = FirebaseFirestore.getInstance();
+
+    }
+
+    private void getDataFromFirebase() {
+
+        mFireStore.collection("HotelItem").get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    if (!queryDocumentSnapshots.isEmpty()) {
+                        List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                        for (DocumentSnapshot snapshot : list) {
+                            HomeModel homeModel = snapshot.toObject(HomeModel.class);
+                            mList.add(homeModel);
+                        }
+                        mHotelAdapter.notifyDataSetChanged();
+                    }
+                });
+
+
+        mHotelAdapter = new HomeAdapter(getActivity(), mList);
+        mHotelRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mHotelRecycler.setAdapter(mHotelAdapter);
+        mHotelAdapter.notifyDataSetChanged();
+    }
+
 }
